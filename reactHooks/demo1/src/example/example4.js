cdd8cec9f0d4b345f3ddbbd9c80d67b4efbba4f4
mike_ma <mike_ma>
2019-10-19 23:16:52 +0800

    dd

---
+++ reactHooks/demo1/src/example/example4.js
@@ -0,0 +1,12 @@
+import React, { useState,useReducer   } from 'react'
+// useReducer介绍和简单使用
+function Example4(){
+  const [count,setCount] = useState(0)
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={()=>{setCount(count+5  )}}>click me</button>   
+    </div>
+  )
+}
+export default Example4
--- reactHooks/demo1/src/index.js
+++ reactHooks/demo1/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import Example1 from './example/example1'
 // import Example2 from './example/example2'
-import Example3 from './example/example3'
+// import Example3 from './example/example3'
+import Example4 from './example/example4'
 
-ReactDOM.render(<Example3 />, document.getElementById('root'));
+ReactDOM.render(<Example4 />, document.getElementById('root'));
 
   

3a33e9bda0ed6cfe2e2ecb039ddae631d34a0838
mike_ma <marui02@zto.com>
2019-11-08 13:07:47 +0800

    day2-1

--- dailyQuestion/day1.js
+++ dailyQuestion/day1.js
@@ -1,24 +1,21 @@
 // call、aplly、bind 实现
 
-Function.prototype.mybind = function (thisArg) {
+Function.prototype.mybind = function(thisArg) {
   if (typeof this !== 'function') {
-    throw TypeError ('Bind must be called on a function');
+    throw TypeError('Bind must be called on a function');
   }
   // 拿到参数，为了传给调用者
-  const args = Array.prototype.slice.call (arguments, 1),
+  const args = Array.prototype.slice.call(arguments, 1),
     // 保存 this
     self = this,
     // 构建一个干净的函数，用于保存原函数的原型
-    nop = function () {},
+    nop = function() {},
     // 绑定的函数
-    bound = function () {
+    bound = function() {
       // this instanceof nop, 判断是否使用 new 来调用 bound
       // 如果是 new 来调用的话，this的指向就是其实例，
       // 如果不是 new 调用的话，就改变 this 指向到指定的对象 o
-      return self.apply (
-        this instanceof nop ? this : thisArg,
-        args.concat (Array.prototype.slice.call (arguments))
-      );
+      return self.apply(this instanceof nop ? this : thisArg, args.concat(Array.prototype.slice.call(arguments)));
     };
 
   // 箭头函数没有 prototype，箭头函数this永远指向它所在的作用域
@@ -26,7 +23,32 @@ Function.prototype.mybind = function (thisArg) {
     nop.prototype = this.prototype;
   }
   // 修改绑定函数的原型指向
-  bound.prototype = new nop ();
+  bound.prototype = new nop();
   return bound;
 };
 // call 实现
+// @ts-ignore
+Function.prototype.myCall = function(thisArg) {
+  // this指向调用call的对象
+  if (typeof this !== 'function') {
+    // 调用call的若不是函数则报错
+    throw new TypeError('Error');
+  }
+
+  const args = [...arguments].slice(1);
+  thisArg = thisArg || window;
+  // 将调用call函数的对象添加到thisArg的属性中
+  thisArg.fn = this;
+  // 执行该属性
+  const result = thisArg.fn(...args);
+  // 删除该属性
+  delete thisArg.fn;
+  // 返回函数执行结果
+  return result;
+};
+
+Function.prototype.call = function(args) {
+  if (typeof this !== 'function') {
+    throw new TypeError('必须是一个函数');
+  }
+};
---
+++ dailyQuestion/day2.js
@@ -0,0 +1,61 @@
+// js动画最佳实现——requestAnimationFrame
+// setInterval和setTimeout刷新率不固定，可能会造成视觉闪动
+
+/**
+ * 1、 requestAnimationFrame 会把每一帧中的所有DOM操作集中起来， 在一次重绘或回流中就完成， 并且重绘或回流的时间间隔紧紧跟随浏览器的刷新频率， 一般来说， 这个频率为每秒60帧。
+ * 2、 在隐藏或不可见的元素中， requestAnimationFrame将不会进行重绘或回流， 这当然就意味着更少的的cpu， gpu和内存使用量。
+ * 3、还可以用来渲染大数据量
+ */
+let dis = 0
+let div = document.getElementById('id')
+
+function step() {
+  requestAnimationFrame(function () {
+    div.style.left = ++dis + 'px';
+    if (dis < 50) step();
+  })
+}
+step()
+
+// DocumentFragment
+/**
+ * DocumentFragment，文档片段接口，表示一个没有父级文件的最小文档对象。它被作为一个轻量版的 Document使用，用于存储已排好版的或尚未打理好格式的XML片段。最大的区别是因为 DocumentFragment不是真实DOM树的一部分，它的变化不会触发DOM树的（重新渲染) ，且不会导致性能等问题。
+  可以使用 document.createDocumentFragment方法或者构造函数来创建一个空的 DocumentFragment
+ */
+
+//需要插入的容器
+
+let ul = document.getElementById('container');
+// 插入十万条数据
+let total = 100000;
+// 一次插入 20 条
+let once = 20;
+//总页数
+let page = total / once
+//每条记录的索引
+let index = 0;
+//循环加载数据
+/**
+ * 
+ * @param {Number} curTotal  当前总条数
+ * @param {Number} curIndex  当前次数
+ */
+function loop(curTotal, curIndex) {
+  if (curTotal <= 0) {
+    return false;
+  }
+  //每页多少条
+  let pageCount = Math.min(curTotal, once);
+  window.requestAnimationFrame(
+    function () {
+      let fragment = document.createDocumentFragment();
+      for (let i = 0; i < pageCount; i++) {
+        let li = document.createElement('li');
+        li.innerText = curIndex + i + ' : ' + ~~(Math.random() * total)
+        fragment.appendChild(li)
+      }
+      ul.appendChild(fragment)
+      loop(curTotal - pageCount, curIndex + pageCount)
+    })
+}
+loop(total, index);

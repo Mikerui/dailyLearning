a41a71cb15bd8c68d8789e793a3d62103f8384ac
mike_ma <marui02@zto.com>
2021-03-02 09:30:21 +0800

    feat: io

---
+++ node/example/index.js
@@ -0,0 +1,15 @@
+const glob = require('glob')
+// 阻塞IO
+console.time('time')
+const result = glob.sync(__dirname, './')
+console.timeEnd('time')
+console.log(result)
+
+// 非阻塞IO
+console.time('async')
+const result2 = glob(__dirname + '/**/*', function (err, result) {
+  console.log(result.length)
+})
+console.timeEnd('async')
+// IO完成之前还可以做别的事
+console.log('hello geekbang')
